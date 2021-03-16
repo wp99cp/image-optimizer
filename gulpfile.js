5575c97a2e426c8646480919634ce958bbd4ba2d
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2021-03-16 21:02:13 +0100

    support for HEIC images

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const options = require('./options.json');
+const clean = require('gulp-clean');
+const convert = require('heic-convert');
+const {promisify} = require('util');
+const fs = require('fs');
+const image = require('gulp-image');
+const scaleImages = require('gulp-scale-images');
+const flatMap = require('flat-map').default;
+const del = require('del');
 
-// user options such as image size
-var options = require('./options.json');
+const INPUT_DIRECTORY = "./input";
+const WORKING_DIRECTORY = "./processing";
+const OUTPUT_DIRECTORY = "./output";
+
+const WORKING_FILES = `${WORKING_DIRECTORY}/*.${generateImageTypeString()}`;
+const INPUT_FILES = `${INPUT_DIRECTORY}/*.${generateImageTypeString()}`;
+
 
 function generateImageTypeString() {
 
@@ -19,22 +33,70 @@ function generateImageTypeString() {
     return inputTypeSting + "}";
 }
 
-const INPUT_DIRECTORY = "./input";
-const INPUT_FILES = `${INPUT_DIRECTORY}/*.${generateImageTypeString()}`;
-const OUTPUT_DIRECTORY = "./output";
+/**
 
-// deleting all files of a directory
-var clean = require('gulp-clean');
+ Starting point for the application.
+ On start the output and working directory gets cleared.
 
-// Deletes all files in the folder /output
-gulp.task('clearOutput', function () {
-    return gulp.src(`${OUTPUT_DIRECTORY}/*`,
-        { read: false })
-        .pipe(clean());
+ */
+gulp.task('default', () => {
+
+    // clear directories
+    gulp.src(`${WORKING_DIRECTORY}/*`, {read: false}).pipe(clean());
+    gulp.src(`${OUTPUT_DIRECTORY}/*`, {read: false}).pipe(clean());
+
+    gulp.watch(INPUT_DIRECTORY + '/*.*', gulp.series('convert_heic', 'copy_files'));
+    gulp.watch(WORKING_FILES, gulp.series('process'));
+
 });
 
+gulp.task('copy_files', (done) =>
+    gulp.src(INPUT_FILES)
+        .pipe(gulp.dest('./processing'))
+        .on('end', function () {
+            del(INPUT_DIRECTORY + '/*.*').then(function () {
+                done();
+            });
+        })
+);
+
+function convert_files(path) {
+
+    try {
+
+        return new Promise(async res => {
+
+            const inputBuffer = await promisify(fs.readFile)('./input/' + path);
+
+            const outputBuffer = await convert({
+                buffer: inputBuffer, // the HEIC file buffer
+                format: 'JPEG',      // output format
+                quality: 1           // the jpeg compression quality, between 0 and 1
+            });
+
+            await promisify(fs.writeFile)('./processing/' + path + '.jpg', outputBuffer);
+
+            res();
+        })
+    } catch {
+        return null;
+    }
+
+}
+
+
+gulp.task("convert_heic", async () => {
+
+    return Promise.all(
+        fs.readdirSync('./input/')
+            .filter(file => file.endsWith('.HEIC'))
+            .map(file => convert_files(file)));
+
+});
+
+
 // duplicate the images
-var processSizes = (image, cb) => {
+var processSizes = async (image, cb) => {
 
     let images = [];
 
@@ -45,6 +107,7 @@ var processSizes = (image, cb) => {
         for (let size of options[sizeList]) {
 
             let imageClone = image.clone()
+
             imageClone.scale = {
                 maxWidth: size,
                 format: options.outputType
@@ -58,35 +121,31 @@ var processSizes = (image, cb) => {
     cb(null, images);
 }
 
-// Optimize Images and scaling
-var image = require('gulp-image');
-var scaleImages = require('gulp-scale-images');
-
-// A flat map implementation for node streams
-var flatMap = require('flat-map').default;
 
 //compress all images
-gulp.task('process', async function () {
+gulp.task('process', async function (done) {
 
     console.log('default');
 
-
-    return gulp.src(INPUT_FILES)
+    return gulp.src(WORKING_FILES)
         .pipe(flatMap(processSizes))
         .pipe(scaleImages())
         .pipe(image())
-	//Remove Space
-    	.pipe(rename(function(opt) {
-      		opt.basename = opt.basename.split(' ').join('_');
-      		return opt;
-    	}))
-        .pipe(gulp.dest(OUTPUT_DIRECTORY));
+        //Remove Space
+        .pipe(rename(function (opt) {
+            opt.basename = opt.basename.split(' ').join('_');
+            return opt;
+        }))
+        .pipe(gulp.dest(OUTPUT_DIRECTORY))
+        .on('end', function () {
+            del(WORKING_FILES).then(function () {
+                done();
+            });
+        });
 
 });
 
-// Default Task watchs for file changes in the folder /input
-gulp.task('default', function () {
 
-    gulp.watch(INPUT_FILES, gulp.series('clearOutput', 'process'));
 
-});
+
+
