b98b22f48e5c85ecfb80d0461b993414f665c5cf
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2021-03-22 20:55:08 +0100

    Release Version 2.0.0
    - support for HEIC
    - basic commandline options
    - delete output directory on restart

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,22 @@ const fs = require('fs');
 const image = require('gulp-image');
 const scaleImages = require('gulp-scale-images');
 const flatMap = require('flat-map').default;
-const del = require('del');
+const tap = require('gulp-tap');
+const inquirer = require("inquirer");
 
 const INPUT_DIRECTORY = "./input";
 const WORKING_DIRECTORY = "./processing";
 const OUTPUT_DIRECTORY = "./output";
 
-const WORKING_FILES = `${WORKING_DIRECTORY}/*.${generateImageTypeString()}`;
-const INPUT_FILES = `${INPUT_DIRECTORY}/*.${generateImageTypeString()}`;
 
-
+/**
+ *
+ * Generates a set of file-names according to the settings.
+ * Containing all natively supported file types listed in the settings,
+ * i.g., JPG, PNG, JPEG, and GIF.
+ *
+ * @return {string}
+ */
 function generateImageTypeString() {
 
     let inputTypeSting = "{";
@@ -34,100 +40,145 @@ function generateImageTypeString() {
 }
 
 /**
+ * List of natively supported file types listed in the settings.
+ * In the WORKING_DIRECTORY (./processing/).
+ * @type {string}
+ */
+const WORKING_FILES = `${WORKING_DIRECTORY}/*.${generateImageTypeString()}`;
+
+/**
+ * List of natively supported file types listed in the settings.
+ * In the INPUT_DIRECTORY (./input/).
+ * @type {string}
+ */
+const INPUT_FILES = `${INPUT_DIRECTORY}/*.${generateImageTypeString()}`;
+
+let sizes = [];
+
+/**
 
  Starting point for the application.
  On start the output and working directory gets cleared.
 
  */
-gulp.task('default', () => {
+gulp.task('default', async () => {
+
+    console.log(`\n\n
+                 *********************************************************
+                 *              Welcome to the image optimizer           *
+                 *            created by Cyrill Püntener v/o JPG         *
+                 *                                                       *
+                 *                    (c) 2019 - 2021                    *
+                 *********************************************************\n\n`)
+
+    console.log('** setup application **')
+
 
     // clear directories
+    console.info('✔  Clear INPUT_DIRECTORY, WORKING_DIRECTORY, and OUTPUT_DIRECTORY.')
+    gulp.src(`${INPUT_DIRECTORY}/*`, {read: false}).pipe(clean());
     gulp.src(`${WORKING_DIRECTORY}/*`, {read: false}).pipe(clean());
     gulp.src(`${OUTPUT_DIRECTORY}/*`, {read: false}).pipe(clean());
 
-    gulp.watch(INPUT_DIRECTORY + '/*.*', gulp.series('convert_heic', 'copy_files'));
-    gulp.watch(WORKING_FILES, gulp.series('process'));
+    // ready for processing files
+    console.info('✔  Ready for processing images.');
+    gulp.watch(INPUT_DIRECTORY + '/*.*', gulp.series('convert_heic', 'copy_files', 'process'));
+
+    console.log('\n \n');
+
+    const answers = await inquirer.prompt([
+        {
+            type: 'checkbox',
+            message: 'Select output formats',
+            name: 'output_format',
+            choices: options.output_formates,
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return 'You must choose at least one output.';
+                }
+
+                return true;
+            },
+        },
+    ])
+
+
+    sizes = options.output_formates
+        .filter(out => answers['output_format'].includes(out.name))
+        .map(out => out.sizes).flat();
+
+    console.log('Selected formats: ', answers['output_format'])
+    console.log('Generated sizes: ', sizes);
 
 });
 
-gulp.task('copy_files', (done) =>
-    gulp.src(INPUT_FILES)
-        .pipe(gulp.dest('./processing'))
-        .on('end', function () {
-            del(INPUT_DIRECTORY + '/*.*').then(function () {
-                done();
-            });
-        })
-);
 
-function convert_files(path) {
-
-    try {
-
-        return new Promise(async res => {
-
-            const inputBuffer = await promisify(fs.readFile)('./input/' + path);
-
-            const outputBuffer = await convert({
-                buffer: inputBuffer, // the HEIC file buffer
-                format: 'JPEG',      // output format
-                quality: 1           // the jpeg compression quality, between 0 and 1
-            });
-
-            await promisify(fs.writeFile)('./processing/' + path + '.jpg', outputBuffer);
-
-            res();
-        })
-    } catch {
-        return null;
-    }
-
-}
-
-
+/**
+ *
+ * Converts images in the apple's HEIC format to JPEG images for
+ * further processing.
+ *
+ */
 gulp.task("convert_heic", async () => {
 
-    return Promise.all(
-        fs.readdirSync('./input/')
-            .filter(file => file.endsWith('.HEIC'))
-            .map(file => convert_files(file)));
+    console.log('\n** Convert files from HEIC to JPEG**')
 
+    return Promise.all(fs.readdirSync('./input/')
+        .filter(path => path.endsWith('.HEIC'))
+        .map(path => new Promise(async res => {
+
+                const inputBuffer = await promisify(fs.readFile)('./input/' + path);
+
+                const outputBuffer = await convert({
+                    buffer: inputBuffer, // the HEIC file buffer
+                    format: 'JPEG',      // output format
+                    quality: 1           // the jpeg compression quality, between 0 and 1
+                });
+
+                await promisify(fs.writeFile)('./processing/' + path + '.jpg', outputBuffer);
+
+                console.log('✔ ' + path + ' converted to JPG')
+
+                try {
+                    fs.unlinkSync('./input/' + path)
+                    //file removed
+                } catch (err) {
+                    console.error(err)
+                }
+
+                res();
+            })
+        ));
 });
 
 
-// duplicate the images
-var processSizes = async (image, cb) => {
-
-    let images = [];
-
-    console.log(options.sizes)
-
-    for (let sizeList of options.sizes)
-
-        for (let size of options[sizeList]) {
-
-            let imageClone = image.clone()
-
-            imageClone.scale = {
-                maxWidth: size,
-                format: options.outputType
-            }
-
-            images.push(imageClone);
-
-        }
+/**
+ * Copies the files into the WORKING_DIRECTORY.
+ *
+ */
+gulp.task('copy_files', () =>
+    gulp.src(INPUT_FILES)
+        .pipe(tap((file) => fs.unlinkSync(file.path)))
+        .pipe(gulp.dest('./processing')));
 
 
-    cb(null, images);
-}
+/**
 
+ Compress the images according to the settings.
+ Creates the different output formates.
 
-//compress all images
-gulp.task('process', async function (done) {
+ */
+gulp.task('process', async function () {
 
-    console.log('default');
+    console.log('\n** Convert files **')
 
     return gulp.src(WORKING_FILES)
+        .pipe(tap((file) => {
+            try {
+                fs.unlinkSync(file.path)
+            } catch {
+            }
+        }))
         .pipe(flatMap(processSizes))
         .pipe(scaleImages())
         .pipe(image())
@@ -137,13 +188,40 @@ gulp.task('process', async function (done) {
             return opt;
         }))
         .pipe(gulp.dest(OUTPUT_DIRECTORY))
-        .on('end', function () {
-            del(WORKING_FILES).then(function () {
-                done();
-            });
-        });
+
 
 });
+
+
+/**
+ * Helper function for scaling the images.
+ * Clones the original file steam and scales the
+ * image to the listed sizes.
+ *
+ * @param image
+ * @param cb
+ * @return {Promise<void>}
+ */
+const processSizes = async (image, cb) => {
+
+    let images = [];
+
+    for (let size of sizes) {
+
+        let imageClone = image.clone()
+
+        imageClone.scale = {
+            maxWidth: size,
+            format: options.outputType
+        }
+
+        images.push(imageClone);
+
+    }
+
+    cb(null, images);
+}
+
 
 
 
